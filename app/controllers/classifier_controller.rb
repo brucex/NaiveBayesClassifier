490b2a6db0d5b82a1a1b5b66bc376fc5f287a25e
Bruce Xu <brucex88@gmail.com>
2015-03-02 17:48:57 -0500

    added person model validation

--- app/controllers/classifier_controller.rb
+++ app/controllers/classifier_controller.rb
@@ -5,6 +5,7 @@ class ClassifierController < ApplicationController
 	end
 
 	def classifierpage
-		@gender = Classifier.classifier(params[:height_field], params[:weight_field])
+		gender = Classifier.classifier( params[:height_field], params[:weight_field] )
+		#@person = Person.new( gender: gender, height: params[:height_field], weight: params[:weight_field] )
 	end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
-	
+	validates :gender, presence: true
+	validates :height, numericality: { greater_than: 0 }
+	validates :weight, numericality: { greater_than: 0 }
 end
